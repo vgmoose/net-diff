365fd03b1dd1b8d2ad181abbc42418e2441ea183
Ricky Ayoub <vgmoose@gmail.com>
2015-04-28 04:51:03 -0400

    Add file loading and initial server/client connection code

--- netdiff.py
+++ netdiff.py
@@ -1,20 +1,30 @@
 #!/bin/python
 
-import os, sys, urllib2
+import os, sys, urllib2, hashlib
 from socket import *
 
 # default port
 port = 3823
 host = False
 
+chunk = 9999
+
 def usage():
     print("on server:\n\tpython %s file [port]" % sys.argv[0])
     print("on client:\n\tpython %s file [hostname] [post]" % sys.argv[0])
     exit()
+    
+def md5(string):
+    m = hashlib.md5()
+    m.update(string)
+    return m.hexdigest()
 
 # first argument is required and is the file to diff
 try:
-    content = open(sys.argv[1], "rb")
+    #read all bytes into memory
+    content = bytearray(open(sys.argv[1], "rb").read())
+    print("\nLoaded %s (%i bytes) into memory!" % (os.path.basename(sys.argv[1]), len(content)))
+    print("Hash: %s\n" % md5(content))
 except:
     usage()
 
@@ -45,19 +55,29 @@ if (len(sys.argv) > 3):
 # ascii support (render text like diff does)
 # work on files of arbitrary sizes, not just the same
 
+def communicate(peer, isclient):
+    pass
+    
 
-## if client
-#if host:
-#    print("Connecting to %s on port %i" % (host, port))
-#    
-#    s = socket(AF_INET, SOCK_STREAM)
-#    s.connect((host, port))
-#    
-#    # receive size
-#    size = s.recv(9999)
-#    s.send(my_name)
-#    
-## if host
-#else:
-#    print("Hosting server on our public IP (%s) on port %i" % (urllib2.urlopen('http://ip.42.pl/raw').read(), port))
-#    
+# if client
+if host:
+    print("Trying to connect to %s on port %i..." % (host, port))
+    
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect((host, port))
+    
+    communicate(s, True)
+    
+# if host
+else:
+    print("Hosting server on our public IP (%s) on port %i" % (urllib2.urlopen('http://ip.42.pl/raw').read(), port))
+    print("Waiting for a client to connect...\n")
+    
+    server = socket(AF_INET, SOCK_STREAM)
+
+    server.bind(('', port))
+    server.listen(0)
+    conn, host = server.accept()
+    
+    communicate(conn, False)
+    
